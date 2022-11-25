ef85dfe112e4ddbefe4afab2676a3aaf3c4235ba
FemeniasM <mmfemenias@gmail.com>
2022-11-25 12:28:07 -0300

    v0.1.0

--- R/mk.reference.r
+++ R/mk.reference.r
@@ -1,4 +1,4 @@
-#' @importFrom dplyr %>% group_by summarize
+#' @import seqinr
 #' @title Makes a reference file for Salmon
 #' @description This function creates decoys and a transcriptome that will be used by Salmon. It also creates a reference file to import the estimates after the Salmon run.
 #' The user can enter a RepMask file without deleting co-transcribed or overlapping repeats with the RepMask argument, or enter a RepMask file without co-transcribed but overlapping repeats with the RepMask.clean argument, or a file free of co-transcribed or overlapping repeats with the RepMask.ovlp.clean argument. When the file contains co-transcribed repeats, it must indicate rm.cotrans = T and when the file contains overlaps it must indicate overlapping = T.
@@ -12,7 +12,7 @@
 #' @param by The column by which the repeats will be classified: 'classRep' (default) or 'namRep'.
 #' @param best.by Defines if only the best match of each transcript/sequence id should be returned (by default best.by = NULL which shows all matches for the sequence). The user can choose whether to be based on the longest repeat length ('total_repeat_length') or the highest percent identity ('per_divergence'). The mk.reference() function uses the best.by argument when references are annotated by transcripts (annot_by = 'transcripts')
 #' A logical vector indicating its only the longest repeats for each transcript is reported. By default best = TRUE
-#' @param custom.lengths A a numerical vector indicating the lengths for each transcript. By default lengths are calculated from the RepeatMasker file.
+#' @param trpt.length A data.frame with two columns: the first column must contain the name of the transcripts, and the second the length corresponding to each transcript. The default is trpt.length=NULL, and the lengths for each transcript are taken from the RepeatMasker file.
 #' @param trme transcriptome in fasta format
 #' @param RepMask RepeatMasker output file. If rm.cotrans = F it is assumed that the file does not contain cotranscribed repeats. If overlapping = F it is assumed that the file does not contain overlapping.
 #' @param rm.cotrnas logical vector indicating whether co-transcribed repeats should be removed
@@ -35,71 +35,91 @@
 #' @param annot_by A character vector indicating whether the annotations should be made by "transcripts" or by "fragments". When annot_by = "transcripts", the proportion of each transposon class/family in each transcript is calculated and the transcript is annotated with the class/family with the highest coverage.
 #' @param ignore.aln.pos The RepeatMasker alignments file may have discrepancies in the repeats positions with respect to the output file. If you selected over.res = "LD", then you can choose whether to take into account the positions of the alignment file or to take the average per repeats class (default).
 #' @param threads Number of cores to use in the processing. By default threads = 1
-#' @param bedtools bedtools binary path
 #' @export
-mk.reference <- function(RepMask,overlapping=F, by="classRep", trme, threads=1, annot_by="transcripts", rule=c(80,80,80), best.by="total_repeat_length", bedtools="bedtools", outdir, over.res="HS",custom.lengths=NULL, ...){
+mk.reference <- function(RepMask,overlapping=T, by="classRep", trme, threads=1, annot_by="transcripts", rule=c(80,80,80), best.by="total_repeat_length", outdir, over.res="HS",trpt.length=NULL, ...){
 
-if(overlapping==T){
-    RM <- ovlp.res(RepMask=RepMask, threads=threads, outdir=outdir,over.res=over.res,...)
-  }else{
-    RM <- RepMask
-  }
+  if(overlapping==T){
+    RM <- ovlp.res(RepMask=RepMask, threads=threads, outdir=outdir,over.res=over.res,trpt.length=trpt.length,by=by,...)
+
+    }else{
+
+      RM <- RepMask
+
+      }
+
 RM <- RM[RM$classRep%!in%c("Unknown", "rRNA", "Satellite", "Simple_repeat","Low_complexity","RNA","scRNA","snRNA","srpRNA", "tRNA","Other"),]
 
-if(annot_by=="transcripts"){
-message("applying the Wicker-like rule ...")
-WRout <- Wickerlike.rule(RM,by=by, rule = rule, best.by = best.by)
-write.table(unique(WRout$SeqID),"targetTEs.txt", quote = F, row.names = F, col.names=F)
+message(paste("[",Sys.time(),"][INFO]","applying the Wicker-like rule ..."))
+WRout <- RM[RM$per_identity>rule[1] & (RM$perc.rep.trpt*100)>rule[2] & RM$total_transcript_length>rule[3],]
 
-message("making references from transcripts annotations ...")
-system(paste0("awk", ' \'BEGIN{while((getline<"targetTEs.txt")>0)l[">"$1]=1}/^>/{f=l[$1]}f\' ',trme," > targetTEs.fa"))
+WRout_uniq_seqNam <- unique(sort(WRout$namSeq))
 
-Ref.salmon <- data.frame(seqID=WRout$SeqID,repID=WRout[,4])
-} else {
+message(paste("[",Sys.time(),"][INFO]","reading fasta files ..."))
+fasta <- seqinr::read.fasta(trme)
 
-if(annot_by=="fragments"){
-message("applying the Wicker-like rule ...")
-WRout <- Wickerlike.rule(RM,by=by, rule = rule)
-BED <- WRout[,1:4]
+message(paste("[",Sys.time(),"][INFO]","defining decoys sequences ..."))
+decoys_fasta <- fasta[names(fasta)%!in%WRout_uniq_seqNam]
+decoys <- names(decoys_fasta)
 
-message("making references from fragments annotations ...")
-write.table(BED,"WRout.bed", quote = F, row.names = F, col.names=F, sep="\t")
+  if(annot_by=="transcripts"){
+    #anotando los transcriptos: como pueden haber mas de una repeticion no solapada en cada transcripto, se usa best.by para anotar solo una rep por secuencia
+    #por defecto "total_repeat_length" selecciona la repeticion que suma mas pb en todo el transcripto
 
-system("cat WRout.bed | sort -k1,1 -k2,2n > RM_sort.bed")
-system(paste(bedtools,"merge -i RM_sort.bed -c 4 -o collapse > RM_sort_merged.bed"))
-system(paste(bedtools,"getfasta -fi",trme,"-bed RM_sort_merged.bed -fo targetTEs.fa"))
+    message(paste("[",Sys.time(),"][INFO]","annotating by transcripts ..."))
 
-Ref.salmon <- data.frame(seqID=paste0(BED[,1],":",BED[,2],"-",BED[,3]),repID=BED[,4])
-} else {
-  message("error in annot_by argument")
+    WRout <- WRout[order(as.numeric(WRout[,best.by]), decreasing = T),]
+    WRout  <- WRout[!duplicated(WRout$namSeq),]
+
+    target_fasta <- fasta[names(fasta)%in%WRout$namSeq]
+
+    message(paste("[",Sys.time(),"][INFO]","Generating salmon references with by =", paste0("'",by,"'")))
+
+    Ref.salmon <- WRout[,c("namSeq",by)]
+
+
+  } else if(annot_by=="fragments"){
+
+    if(by=="namRep"){
+      WRout$SeqID <- paste0(WRout$namSeq,":",WRout$start,":",WRout$end,":",WRout$namRep,"/",WRout$classRep)
+    }
+    if(by=="classRep"){
+      WRout$SeqID <- paste0(WRout$namSeq,":",WRout$start,":",WRout$end,":",WRout$classRep)
+    }
+
+      BED <- WRout[,c("namSeq","start","end","SeqID")]
+
+      target_fasta <- list()
+
+      for(i in 1:nrow(BED)){
+        target_fasta[[i]] <- seqinr::as.SeqFastadna(fasta[[BED$namSeq[i]]][BED$start[i]:BED$end[i]],
+                                                         name = BED$SeqID[i],
+                                                         Annot = BED$SeqID[i])
+
+      }
+      message(paste("[",Sys.time(),"][INFO]","Generating salmon references with by =", paste0("'",by,"'")))
+
+      Ref.salmon <- WRout[,c("SeqID",by)]
+
   }
-}
 
-message("taking decoy sequences")
-allIDfasta <- system(paste("grep '>'",trme,"|sed 's/>//' | awk '{print $1}'"),intern = T)
-decoys <- allIDfasta[allIDfasta%!in%unique(WRout$SeqID)]
-write.table(decoys,"decoys.temp", col.names = F, row.names = F, quote = F)
+all.fasta <- c(target_fasta,decoys_fasta)
 
-system(paste0("awk", ' \'BEGIN{while((getline<"decoys.temp")>0)l[">"$1]=1}/^>/{f=l[$1]}f\' ',trme," > decoy.fa"))
+  if(!(dir.exists(outdir))){
 
-message("making trmeSalmon.fasta file")
-system("cat targetTEs.fa decoy.fa > trmeSalmon.fasta")
+    dir.create(outdir)
+  }
 
-if(annot_by=="transcripts"){
-system("rm targetTEs.fa decoy.fa decoys.temp")
-}
+message(paste("[",Sys.time(),"][INFO]","writing references files in the output directory..."))
 
-if(annot_by=="fragments"){
-system(paste("rm WRout.bed targetTEs.fa RM_sort_merged.bed RM_sort.bed decoy.fa decoys.temp", paste0(trme,".fai")))
-}
+seqinr::write.fasta(sequences = seqinr::getSequence(all.fasta), names = seqinr::getName(all.fasta), file.out = paste0(outdir,"/trmeSalmon.fasta"))
 
-message("writing files in the output directory...")
-system(paste("mv trmeSalmon.fasta",outdir))
 write.table(Ref.salmon,paste0(outdir,"/references.csv"), col.names = F, row.names = F, quote = F, sep = ";")
+
 write.table(decoys,paste0(outdir,"/decoys.txt"), col.names = F, row.names = F, quote = F)
 
-message(paste("The reference.csv, decoys.txt and trmeSalmon.fasta files are in", outdir, "directory"))
 
-  Ref.salmon
+message(paste("[",Sys.time(),"][INFO]","The reference.csv, decoys.txt and trmeSalmon.fasta files are in", outdir, "directory"))
 
+Ref.salmon
 }
+
